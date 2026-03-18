68bc8f5ae5998ba4569c34d15e8f64b979581fac
Qiling Tan <72065263+professorqtaku@users.noreply.github.com>
2026-03-18 09:46:54 +0100

    add constructor Expense and Income

--- MoneyTrackingApplication/components/Expense.cs
+++ MoneyTrackingApplication/components/Expense.cs
@@ -1,6 +1,11 @@
-﻿namespace Money_Tracking_Application.components
+﻿using MoneyTrackingApplication.utils;
+
+namespace Money_Tracking_Application.components
 {
     internal class Expense : Item
     {
+        public Expense(string title, float amount, Month? month) : base(title, amount, month)
+        {
+        }
     }
 }
