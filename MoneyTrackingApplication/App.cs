7f7decbcb75a107c754d519f2b5b8ee39cd10b55
Qiling Tan <72065263+professorqtaku@users.noreply.github.com>
2026-04-01 01:36:29 +0200

    clear for runs after 1st run

--- MoneyTrackingApplication/App.cs
+++ MoneyTrackingApplication/App.cs
@@ -5,18 +5,35 @@ namespace Money_Tracking_Application
 {
     internal class App
     {
-        private ItemManager _itemManager = new ItemManager();
+        private ItemService _itemManager = new ItemService();
         public void Run()
         {
+            bool firstRun = true;
+            WelcomeMessage();
             while (true)
             {
+                if(firstRun)
+                {
+                    firstRun = false;
+                }
+                else
+                {
+                    Console.Clear();
+                }
                 Menu.ShowMainMenu();
                 var input = Menu.GetInput();
                 MessageHandler.ShowMessage($"You entered: {input}", ConsoleColor.Green);
                 HandleFirstChoie(input);
                 Console.WriteLine();
             }
-}
+        }
+        private void WelcomeMessage()
+        {
+            MessageHandler.ShowMessage("Welcome to TrackMoney");
+            double total = _itemManager.GetTotalAmount();
+            string presentation = total > 0 ? total.ToString() : "-";
+            MessageHandler.ShowMessage($"You have currently ({presentation}) kr on your account.");
+        }
 
         // This method gets the input from user and handles the choices in a switch statement, it is called in the Run method
         private void HandleFirstChoie(string input)
@@ -24,13 +41,13 @@ namespace Money_Tracking_Application
             switch (input)
             {
                 case "1":
-                    _itemManager.HandleDisplayItems();
+                    _itemManager.HandleDisplay();
                     break;
                 case "2":
-                    _itemManager.HandleAddItem();
+                    _itemManager.HandleAdd();
                     break;
                 case "3":
-                    //_itemManager.DeleteItem();
+                    _itemManager.HandleSearchAndEdit();
                     break;
                 case "4":
                     //_itemManager.UpdateItem();
